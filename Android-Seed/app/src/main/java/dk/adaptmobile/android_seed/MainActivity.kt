0ae0d05541a469235313d90e749e75838309e088
Christian Steffensen <christian.steffensen@adaptagency.com>
2019-10-10 00:16:40 +0200

    final cleanup

--- Android-Seed/app/src/main/java/dk/adaptmobile/android_seed/MainActivity.kt
+++ Android-Seed/app/src/main/java/dk/adaptmobile/android_seed/MainActivity.kt
@@ -1,29 +1,30 @@
 package dk.adaptmobile.android_seed
 
 import android.os.Bundle
-import com.bluelinelabs.conductor.Conductor
-import dk.adaptmobile.amkotlinutil.navigation.NavManager
+import dk.adaptmobile.android_seed.MainActivityViewModel.Input
 import dk.adaptmobile.android_seed.base.RemoteConfigActivity
-import dk.adaptmobile.android_seed.screens.Routing
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : RemoteConfigActivity() {
 
+    private val viewModel = MainActivityViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mainRouter = Conductor.attachRouter(this, mainContainer, savedInstanceState)
-        modalRouter = Conductor.attachRouter(this, modalContainer, savedInstanceState)
-        modalRouter.setPopsLastView(true) //We want it to be possible to pop the last view on the modal controller only
+        super.setup(mainContainer, modalContainer, savedInstanceState)
 
-        handleRouting()
-
-        NavManager.open(Routing.Start())
+        viewModel.input.onNext(Input.Init)
     }
 
     override fun showMinimumVersionDialog() {
         super.showMinimumVersionDialog()
-//        NavManager.openModally(Routing.MinimumVersionDialog())
+        viewModel.input.onNext(Input.MinimumVersionDialog)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewModel.input.onNext(Input.OnDestroy)
     }
 
 }
---
+++ Android-Seed/app/src/main/java/dk/adaptmobile/android_seed/base/Dependencies.kt
@@ -0,0 +1,8 @@
+package dk.adaptmobile.android_seed.base
+
+import io.reactivex.subjects.PublishSubject
+
+object Dependencies{
+    val noNetworkSubject = PublishSubject.create<Boolean>()
+    val loadingSubject = PublishSubject.create<Boolean>()
+}
