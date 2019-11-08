47feb0cab6f0c519dd3b0c5246dfa6a7276afd9d
Martin L. Jensen <martinloesethjensen@gmail.com>
2019-11-08 11:45:51 +0100

    :construction: Working on find and replace in more files in dir

--- application.py
+++ application.py
@@ -8,60 +8,77 @@ APP_BUILD_GRADLE = "/app/build.gradle"
 APP_NAME_CHANGE = "APP_NAME_CHANGE"
 KEYSTORE_PASSWORD = "KEYSTORE_PASSWORD"
 KEYSTORE_ALIAS = "KEYSTORE_ALIAS"
-PACKAGE_NAME = "dk.adaptmobile.android_seed"
+SEED_PACKAGE_NAME = "dk.adaptmobile.android_seed"
 APP_JAVA_PATH = "/app/src/main/java"
 
 
-def find_and_replace(new, old, file_name):
+def find_and_replace(find, replace, file_name):
     with fileinput.FileInput(file_name, inplace=True) as file:
         for line in file:
-            print(line.replace(old, new), end='')
+            print(line.replace(replace, find), end='')
 
 
-def generate_keystore(alias, password):
+def find_replace_in_dir(directory, find, replace):
+    for path, dirs, files in os.walk(os.path.abspath(directory)):
+        for filename in files:
+            filepath = os.path.join(path, filename)
+            with open(filepath) as file:
+                file_text = file.read()
+            file_text = file_text.replace(find, replace)
+            with open(filepath, "w") as file:
+                file.write(file_text)
+
+
+# This generates the keystore without the keytool cli.
+def generate_keystore():
+    os.chdir("launch")
     cmd = f'keytool -genkey -v -keystore release.keystore -keyalg RSA -keysize 16384 -validity 10000 -alias {alias} -dname "cn=Unknown, ou=Unknown, o=Unknown, c=Unknown" -storepass {password} -keypass {password}'
     args = shlex.split(cmd)
     subprocess.Popen(args)
+    os.chdir("..")
 
 
 def rename_package_dirs():
-    package_name = input("Enter new package name like -> dk.adaptmobile.android_seed: ")
-
     os.chdir(root_module + APP_JAVA_PATH)
 
-    old_package_name_split = PACKAGE_NAME.split(".")
+    seed_package_name_split = SEED_PACKAGE_NAME.split(".")
     new_package_name_split = package_name.split(".")
 
-    for i in range(len(old_package_name_split)):
-        os.rename(old_package_name_split[i], new_package_name_split[i])
+    for i in range(len(seed_package_name_split)):
+        os.rename(seed_package_name_split[i], new_package_name_split[i])
         os.chdir(new_package_name_split[i])
+
+    os.chdir('../../../../../../..')
+    find_replace_in_dir(os.getcwd(), SEED_PACKAGE_NAME, package_name)
+    os.chdir('..')
 
 
 def rename_app_name():
-    app_name = input("What's the app name: ")
+    find_and_replace(find=app_name, replace=APP_NAME_CHANGE, file_name=root_module + APP_BUILD_GRADLE)
+    print(f'Changed app_name to {app_name}')
 
-    find_and_replace(new=app_name, old=APP_NAME_CHANGE, file_name=root_module + APP_BUILD_GRADLE)
+
+def rename_root_module():
+    os.rename("Android-Seed", root_module)
+    print(f'Renamed root module to {root_module}')
+
+
+def rename_keystore_fields():
+    find_and_replace(find=alias, replace=KEYSTORE_ALIAS, file_name=root_module + APP_BUILD_GRADLE)
+    find_and_replace(find=password, replace=KEYSTORE_PASSWORD, file_name=root_module + APP_BUILD_GRADLE)
 
 
 if __name__ == '__main__':
-    print("The dir is: %s" % os.listdir(os.getcwd()))
-
     root_module = input("Name root module: ")
+    rename_root_module()
 
-    os.rename("Android-Seed", root_module)
-
-    print("Renamed root module")
-
+    app_name = input("What's the app name: ")
     # rename_app_name()
 
-    os.chdir("launch")
+    package_name = input("Enter new package name like -> dk.adaptmobile.android_seed: ")
+    rename_package_dirs()
+
     alias = input("Alias for the keystore: ")
     password = input("Password for the keystore: ")
-    generate_keystore(alias, password)
-    os.chdir("..")
-    
-    find_and_replace(new=alias, old=KEYSTORE_ALIAS, file_name=root_module + APP_BUILD_GRADLE)
-    find_and_replace(new=password, old=KEYSTORE_PASSWORD, file_name=root_module + APP_BUILD_GRADLE)
-
-
-    # rename_package_dirs()
+    # rename_keystore_fields()
+    # generate_keystore()  # Should be run at the end
